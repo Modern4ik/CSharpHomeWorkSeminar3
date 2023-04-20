a2074a64d14a9a4260ad84c8ac92fe52502e8487
Sergey Zaytsev <surgery_333@mail.ru>
2023-04-20 19:38:04 +0300

    Task23 file added and task solved

---
+++ Task23_NumberCubes/Program.cs
@@ -0,0 +1,30 @@
+﻿// Поиск кубов чисел в заданном пределе N.
+
+Console.Clear();
+
+int numb = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!Повторите попытку!");
+GetCubesByUserNumber(numb);
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int GetCubesByUserNumber(int number)
+{
+    double cubes;
+    for (int i = 1; i <= number; i++)
+    {
+        cubes = Math.Pow(i, 3);
+        Console.Write($"{cubes} ");
+    }
+    return number;
+}
