98e3011cf2179e46c730d20b6044e3de8a49546c
Sergey Zaytsev <surgery_333@mail.ru>
2023-04-20 15:34:53 +0300

    File Task19 added and task solved

---
+++ Task19_Palindrome/Program.cs
@@ -0,0 +1,41 @@
+﻿// Проверка пятизначного числа на палиндром.
+
+Console.Clear();
+int numb = GetNumberFromUser("Введите пятизначное число: ", "Ошибка ввода!Повторите попытку!");
+CheckNumberForPalindrome(numb);
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber) && userNumber > 9999 && userNumber < 100000)
+            return userNumber;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int CheckNumberForPalindrome(int number)
+{
+    int firstHalfOfNumber = number / 1000;
+    int secondHalfOfNumber = number % 100;
+    int numb1 = firstHalfOfNumber / 10;
+    int numb2 = firstHalfOfNumber % 10;
+    int numb3 = secondHalfOfNumber / 10;
+    int numb4 = secondHalfOfNumber % 10;
+
+    if (numb1 == numb4 && numb2 == numb3)
+    {
+        Console.WriteLine($"{number} - является палиндромом.");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - не является палиндромом.");
+    }
+    return number;
+}
